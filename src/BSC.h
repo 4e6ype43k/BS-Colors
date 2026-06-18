9c67e86fcb83947e131637f59f4b7307e012401b
4e6ype43k <zelevan@outlook.com>
2026-06-18 21:01:14 +0100

    added some functions e

--- src/BSC.h
+++ src/BSC.h
@@ -29,21 +29,39 @@ IN THE SOFTWARE.
 */
 
 //? instead of using a whole byte for each color channel, what if we used a whole byte for ALL the channels?
-//* BS (Byte Size) format is similar to that of CIMG, however, every pixel is stored in 1 byte
+//* BSC (Byte-Size Color) format is similar to that of CIMG, however, every pixel is stored in 1 byte
 //* The bits in the byte represent the following: RRGGGBBA (fun fact: human eyes are the most sensitive to green, so that's the reason why the green channel appears here the most)
 //? (yes i know that introduction of the alpha channel halves the amount of colors that could be represented but trust me, its wort it)
 
-#ifndef BITE_SIZE
-#define BITE_SIZE
+#ifndef BITE_SIZE_COLOR
+#define BITE_SIZE_COLOR
 
 #include <stdio.h>
 #include <stdint.h>
+#include <malloc.h>
 
-typedef int8_t BS_color; // it is recommended to use this type instead of any other because i said so
+typedef uint8_t BSC_color; // it is recommended to use this type instead of any other because i said so
 
-typedef struct BS_PD {
+typedef struct BSC_PD {
     uint16_t width, height;
-    BS_color* data; // this pointer is 8 times larger than just one pixel... glup..
-} BS_PD;
+    BSC_color* pixels; // this pointer is 8 times larger than just one pixel... glup..
+} BSC_PD; // stands for Byte-Size Color Pixel Data
+
+void allocBytePixelMemory(BSC_PD* data) {
+    data->pixels=(BSC_color*) calloc(data->width*data->height,1);
+} // same reason for calloc as in CIMG
+
+void freeBytePixelMemory(BSC_PD* data) {
+    free(data->pixels);
+}
+
+// allows printing colors as normal hex....
+void printColor(BSC_color color) {
+    int8_t r,g,b; // a has only 2 states
+    r=color>>6;
+    g=(color>>3)-(r<<3);
+    b=(color>>1)-(r<<5)-(g<<2);
+    printf("0x%02x%02x%02x%02x",r*85,g*36,b*85,(((uint8_t) (color<<7))>>7)*255);
+}
 
 #endif
