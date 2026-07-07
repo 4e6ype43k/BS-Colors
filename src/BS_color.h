4144fccf2fd0744a8e95786905cc82dbb4b8b863
4e6ype43k <zelevan@outlook.com>
2026-07-07 17:34:27 +0100

    Did a lot of stuff and optimizations, and started working on size manipulation thingamajigs (no i didnt)

--- src/BS_color.h
+++ src/BS_color.h
@@ -67,23 +67,76 @@ void freeBytePixelMemory(BSC_PD* data) {
     free(data->pixels);
 }
 
+#ifndef BSC_LUT // if defined, uses a LUT for the values (making the program run faster ig)
 // for now, only converts clr to ARGB 4-byte int
 int32_t BS_colorToInt(BS_color clr){
     if (clr==INVISIBLE_COLOR) return 0; // == is good enough for comparison
     uint8_t r,g,b; // alpha channel is constant, remember?
-    r=clr>>5;
-    g=(clr>>2)-(r<<3);
-    b=clr-(r<<5)-(g<<2);
+    r=clr>>5; // last 5 bits are for g and b
+    g=(clr>>2)&7; // 7 is 0b111, anding the shifted color with it will only give bits where 7 bits are high, ignoring all other bits after
+    b=clr&3; // 3 is 0b11
 
-    if (r*36==252) r=255;
-    else r*=36;
-    if (g*36==252) g=255;
-    else g*=36;
+    uint8_t four; // i have a long explanation to this part but it is too long to be written
+		  // TODO actually explain all this horrid code
+		  // but for now, enjoy the magic
+	four=r<<2;
+	r<<=5;
+	r|=four;
 
-    return (r<<24)+(g<<16)+(b<<8)*85+255; // uhhh
+	four=g<<2;
+	g<<=5;
+	g|=four;
+
+	if (r==252) r=255; // translation error checking (turns out 255%7!=0 (this gives a very vague description of what just happened))
+	if (g==252) g=255;
+
+	// no more crazy bitwise logic
+    return ((r<<24)|(g<<16)|((b<<8)*85))|255; // oring the numbers rather than adding as the byte the channels are writing to are GUARANTEED to be 0
+}
+#else
+
+uint8_t rgLUT={0,36,72,108,144,180,216,255}; // the amount of bits r and g channels occupy are the same, so creating two different LUTs for both would be wasteful (im bad at explaining stuff, ok?)
+uint8_t bLUT={0,85,170,255};
+
+int32_t BS_colorToInt(BS_color clr) {
+	if (clr==INVISIBLE_COLOR) return 0;
+	uint8_t r,g,b;
+	r=clr>>5; // same magic as before
+	g=(clr>>2)&7;
+	b=clr&3;
+	return ((rgLUT[r]<<24)|(rgLUT[g]<<16)|(bLUT[b]<<8))|255; // same thing with or but now with LUTs
 }
 
-// allows printing colors as normal hex....
+// adds two colors (literally just adds the channel values and if they overflow, just sets them to the max values)
+BS_color addBS_colors(BS_color clr0, BS_color clr1) {
+	if (clr0==INVISIBLE_COLOR) return clr1; // idk
+	if (clr1==INVISIBLE_COLOR) return clr0; // idk 2: electric boogaloo
+
+	uint8_t r0,g0,b0; // for clr0
+	r0=clr0>>5;
+	g0=(clr0>>2)&7;
+	b0=clr0&3;
+
+	uint8_t r1,g1,b1; // clr1
+	r1=clr1>>5;
+	g1=(clr1>>2)&7;
+	b1=clr1&3;
+
+	uint8_t r,g,b; // for the output color
+	r=r0+r1;
+	g=g0+g1;
+	b=b0+b1;
+
+	if (r>7) r=7; // i dont have any bitwise tricks for this... so sad.. .
+	if (g>7) g=7;
+	if (b>3) b=3;
+
+	return (r<<5)|(g<<2)|b; // but i have some for this!
+}
+
+#endif
+
+// allows printing colors as 4 byte (or 8 nybble (or nibble)) hex....
 void printBS_color(BS_color clr) {
     int32_t a=BS_colorToInt(clr);
     if (a==0) { // if clr==that color, the func will return 0
@@ -97,37 +150,62 @@ void printBS_color(BS_color clr) {
     printf("0x%02x%02x%02xff",r,g,b); // alpha channel will always be FF but for one case
 }
 
-// loads file into PD
-void loadFileToPD(BSC_PD* data,int8_t* path){
+// loads a file to a new PD
+BSC_PD loadFile(int8_t* path) {
 	FILE* pFile=fopen(path,"rb");
 	uint8_t sizeData[4];
 	fread(sizeData,1,4,pFile);
 	
-	data->width=(sizeData[0]<<8)+sizeData[1];
-	data->height=(sizeData[2]<<8)+sizeData[3];
+	uint16_t width=(sizeData[0]<<8)|sizeData[1];
+	uint16_t height=(sizeData[2]<<8)|sizeData[3];
+	BSC_PD out={width,height}; // no need to specify NULL for pixels as does it for me (i love c)
+	allocBytePixelMemory(&out);
 	
+	int16_t chr=1; // a
+	for (uint32_t x=0;;x++) { // for is apparently faster than while
+		chr=fgetc(pFile);
+		if (chr==EOF) break;
+		out.pixels[x]=chr;
+	}
+
+	fclose(pFile);
+	return out;
+}
+
+// loads file into an existant PD and returns bytes required to store all the pixels
+int32_t loadFileToPD(BSC_PD* data,int8_t* path){
+	FILE* pFile=fopen(path,"rb");
+	uint8_t sizeData[4];
+	fread(sizeData,1,4,pFile);
+	
+	uint16_t width=(sizeData[0]<<8)|sizeData[1]; // wait a second, i can do this here too!
+	uint16_t height=(sizeData[2]<<8)|sizeData[3];
+	data->width=width;
+	data->height=height;
+
 	int16_t chr=40; // this is, in fact, a random number
 	for (uint32_t x=0;;x++) {
-		chr=fgetc(pFile);
+		chr=fgetc(pFile); // fread offsets the char* in FILE
 		if (chr==EOF) break;
 		data->pixels[x]=chr;
 	}
 
 	fclose(pFile);
+	return width*height;
 }
 
 // stores data in path
 void encodePD(BSC_PD* data,int8_t* path){
 	FILE* pFile=fopen(path,"wb");
-	int8_t a=data->width%256;
-	int8_t b=(data->width-a)/256;
-	fputc(b,pFile);
-	fputc(a,pFile);
+	int8_t low=data->width; // assigning a 2-byte value to a 1-byte var makes it store only the low byte (probably because they just access the value through the 8-bit registers)
+	int8_t high=(data->width)>>8; // moving it left 8 times is the same as dividing by 256 (2**8=256)
+	fputc(high,pFile);
+	fputc(low,pFile);
 	
-	a=data->height%256;
-	b=(data->height-a)/256;
-	fputc(b,pFile);
-	fputc(a,pFile);
+	low=data->height;
+	high=(data->height)>>8;
+	fputc(high,pFile);
+	fputc(low,pFile);
 
 	for (uint32_t index=0;index<data->width*data->height;index++) {
 		fputc(data->pixels[index],pFile);
@@ -135,5 +213,11 @@ void encodePD(BSC_PD* data,int8_t* path){
 
 	fclose(pFile);
 }
+
+#ifdef BSC_SIZE_MANIPULATION // alows manipulation of PD size
+
+
+
+#endif
 
 #endif
